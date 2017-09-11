12765366ee3f966d12083f765df7fb660510bd99
Joshua Moore <joshuamoore235@gmail.com>
2017-09-10 21:09:21 -0600

    Added client image handling (input not complete)

--- src/jrdp/Client.java
+++ src/jrdp/Client.java
@@ -1,23 +1,60 @@
 package jrdp;
 
 import java.net.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.swing.*;
 
-public class Client {
+public class Client extends JFrame {
 	private String address,password;
 	private int port;
 	private double compression;
 	
 	private Socket socket;
+	private ObjectInputStream sInput;
+	private ObjectOutputStream sOutput;
+	public JLabel label;
 	
 	public Client(String theAddress,int thePort,String thePassword,double theCompression) {
 		address = theAddress;
 		port = thePort;
 		password = thePassword;
 		compression = theCompression;
+		
+		label = new JLabel(new ImageIcon());
+		add(label);
+		
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setUndecorated(true);
+		setVisible(true);
 	}
 	
 	public void connect() {
-		
+		try {
+			socket = new Socket(address, port);
+			sInput  = new ObjectInputStream(socket.getInputStream());
+			sOutput = new ObjectOutputStream(socket.getOutputStream());
+			sOutput.writeObject(password);
+		}catch(Exception ex) {ex.printStackTrace();}
+		new ListenFromServer(socket,sInput,sOutput).start();
 	}
 	
+	class ListenFromServer extends Thread {
+		Socket socket;
+		ObjectInputStream sInput;
+		ObjectOutputStream sOutput;
+		
+		ListenFromServer(Socket thesocket,ObjectInputStream thesInput,ObjectOutputStream thesOutput){
+			socket = thesocket;sInput = thesInput;sOutput = thesOutput;
+		}
+		
+		public void run() {
+			while(true) {
+				try {
+					BufferedImage screenshot = (BufferedImage) sInput.readObject();
+					label = new JLabel(new ImageIcon(screenshot));
+				}catch(Exception ex) {ex.printStackTrace();}
+			}
+		}
+	}
 }
